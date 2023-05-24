c4719da396831832987c168754885552c179b0d1
DFG <cemka5@mail.ru>
2023-05-24 20:15:45 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,8 +4,13 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
 
+        names[0] = "Petr Seventeen";
+        names[1] = "Grigori Sealskin";
+        names[2] = "Vladimir Koch";
+        names[3] = "Vyacheslav Bearskin";
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
